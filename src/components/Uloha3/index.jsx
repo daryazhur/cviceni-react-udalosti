f64291e461f3b7d76c553c3a3520734d090fcc39
daryazhur <daryazhur@tut.by>
2024-10-10 14:19:38 +0200

    reseni

--- src/components/Uloha3/index.jsx
+++ src/components/Uloha3/index.jsx
@@ -9,30 +9,39 @@ Zadání: Doplňte komponentu tak, aby každé tlačítko po kliknutí vypsalo, 
 */
 
 export const Uloha3 = () => {
+
+  const handlePodzim = () => {
+    alert('Uživatel zvolil podzim')
+  }
+
+  const handleZima = () => {
+    alert('Uživatel zvolil zimu')
+  }
+
   return (
     <>
       Které roční období je ve střední Evropě nejteplejší?
       <ol>
         <li>
-          <button>
+          <button onClick={() => { alert('Uživatel zvolil jaro') }}>
             <img src={kvetinaUrl} width={16} height={16} alt="květina" />
             jaro (březen až květen)
           </button>
         </li>
         <li>
-          <button>
+          <button onClick={() => { alert('Uživatel zvolil léto') }}>
             <img src={slunceUrl} width={16} height={16} alt="slunce" />
             léto (červen až srpen)
           </button>
         </li>
         <li>
-          <button>
+          <button onClick={handlePodzim}>
             <img src={listUrl} width={16} height={16} alt="list" />
             podzim (září až listopad)
           </button>
         </li>
         <li>
-          <button>
+          <button onClick={handleZima}>
             <img src={vlockaUrl} width={16} height={16} alt="vločka" />
             zima (prosinec až únor)
           </button>
--- src/components/Uloha4/index.jsx
+++ src/components/Uloha4/index.jsx
@@ -6,5 +6,11 @@ Zadání 2: Pokud uživatel z textového políčka vše smaže, vypište „Uži
 */
 
 export const Uloha4 = () => {
-  return <input />;
+ 
+  return <input
+            onChange={(event) => {
+              const {value} = event.target
+              console.log(value ? `Uživatel napsal: ${value}` : 'Uživatel nenapsal nic')
+            }}
+        />;
 };
